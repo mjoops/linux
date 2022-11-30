55ea9397782544d71e65bdd8fe20b2243c19ee9c
Sven Peter <sven@svenpeter.dev>
2022-11-30 22:11:16 +0100

    usb: typec: tipd: Move struct tps6598x to header
    Move the tps6598x struct and register defines to tps6598x.h to prepare
    for adding DisplayPort alternate mode support which will live in its
    own file.
    
    Signed-off-by: Sven Peter <sven@svenpeter.dev>

--- drivers/usb/typec/tipd/tps6598x.h
+++ drivers/usb/typec/tipd/tps6598x.h
@@ -8,6 +8,13 @@
 
 #include <linux/bits.h>
 #include <linux/bitfield.h>
+#include <linux/interrupt.h>
+#include <linux/mutex_types.h>
+#include <linux/power_supply.h>
+#include <linux/usb/typec.h>
+#include <linux/usb/typec_altmode.h>
+#include <linux/usb/typec_dp.h>
+#include <linux/workqueue.h>
 
 #ifndef __TPS6598X_H__
 #define __TPS6598X_H__
@@ -131,6 +138,7 @@
 #define TPS_REG_INT_PD_SOFT_RESET			BIT(0)
 
 /* Apple-specific TPS_REG_INT_* bits */
+#define APPLE_CD_REG_INT_DP_SID_STATUS_UPDATE		(BIT_ULL(36) | BIT_ULL(37))
 #define APPLE_CD_REG_INT_DATA_STATUS_UPDATE		BIT(10)
 #define APPLE_CD_REG_INT_POWER_STATUS_UPDATE		BIT(9)
 #define APPLE_CD_REG_INT_STATUS_UPDATE			BIT(8)
@@ -263,5 +271,128 @@
 /* Int Event Register length */
 #define TPS_65981_2_6_INTEVENT_LEN             8
 #define TPS_65987_8_INTEVENT_LEN               11
+
+/* Register offsets */
+#define TPS_REG_VID			0x00
+#define TPS_REG_MODE			0x03
+#define TPS_REG_CMD1			0x08
+#define TPS_REG_DATA1			0x09
+#define TPS_REG_VERSION			0x0F
+#define TPS_REG_INT_EVENT1		0x14
+#define TPS_REG_INT_EVENT2		0x15
+#define TPS_REG_INT_MASK1		0x16
+#define TPS_REG_INT_MASK2		0x17
+#define TPS_REG_INT_CLEAR1		0x18
+#define TPS_REG_INT_CLEAR2		0x19
+#define TPS_REG_SYSTEM_POWER_STATE	0x20
+#define TPS_REG_STATUS			0x1a
+#define TPS_REG_SYSTEM_CONF		0x28
+#define TPS_REG_CTRL_CONF		0x29
+#define TPS_REG_BOOT_STATUS		0x2D
+#define TPS_REG_POWER_STATUS		0x3f
+#define TPS_REG_PD_STATUS		0x40
+#define TPS_REG_RX_IDENTITY_SOP		0x48
+#define TPS_REG_DP_SID_CONFIG		0x51
+#define TPS_REG_DP_SID_STATUS		0x58
+#define TPS_REG_TX_VDM			0x4d
+#define TPS_REG_DATA_STATUS		0x5f
+#define TPS_REG_SLEEP_CONF		0x70
+
+/* TPS_REG_SYSTEM_CONF bits */
+#define TPS_SYSCONF_PORTINFO(c)		((c) & 7)
+
+enum {
+	TPS_PORTINFO_SINK,
+	TPS_PORTINFO_SINK_ACCESSORY,
+	TPS_PORTINFO_DRP_UFP,
+	TPS_PORTINFO_DRP_UFP_DRD,
+	TPS_PORTINFO_DRP_DFP,
+	TPS_PORTINFO_DRP_DFP_DRD,
+	TPS_PORTINFO_SOURCE,
+};
+
+/* TPS_REG_RX_IDENTITY_SOP */
+struct tps6598x_rx_identity_reg {
+	u8 status;
+	struct usb_pd_identity identity;
+} __packed;
+/* TPS_REG_DP_SID_CONFIG */
+struct tps6598x_dp_sid_config {
+#define TPS_DP_SID_ENABLE_DP_SID BIT(0)
+#define TPS_DP_SID_ENABLE_DP_MODE BIT(1)
+	u8 config;
+	u8 capabilities;
+	u8 dfp_d_assignments;
+	u8 ufp_d_assignments;
+	u8 multifunction_config;
+	u8 autoentry_config;
+} __packed;
+
+/* TPS_REG_DP_SID_STATUS */
+struct tps6598x_dp_sid_status {
+#define TPS_DP_SID_DETECTED BIT(0)
+#define TPS_DP_SID_ACTIVE BIT(1)
+	u8 status;
+	__le32 dp_status_tx;
+	__le32 dp_status_rx;
+	__le32 dp_configure;
+	__le32 dp_mode;
+} __packed;
+
+struct tps6598x;
+
+struct tipd_data {
+	irq_handler_t irq_handler;
+	int (*register_port)(struct tps6598x *tps, struct fwnode_handle *node);
+	void (*trace_power_status)(u16 status);
+	void (*trace_status)(u32 status);
+	int (*apply_patch)(struct tps6598x *tps);
+	int (*init)(struct tps6598x *tps);
+	int (*reset)(struct tps6598x *tps);
+};
+
+struct tps6598x {
+	struct device *dev;
+	struct regmap *regmap;
+	struct mutex lock; /* device lock */
+	u8 i2c_protocol:1;
+	u8 cd321x:1; /* enable Apple CD321x quirks */
+	u8 cd321x_unlocked:1;
+
+	struct gpio_desc *reset;
+	struct typec_port *port;
+	struct typec_partner *partner;
+	struct usb_pd_identity partner_identity;
+	struct usb_role_switch *role_sw;
+	struct typec_capability typec_cap;
+
+	struct power_supply *psy;
+	struct power_supply_desc psy_desc;
+	enum power_supply_usb_type usb_type;
+
+	int wakeup;
+	u32 status; /* status reg */
+	u16 pwr_status;
+	struct delayed_work	wq_poll;
+
+	const struct tipd_data *data;
+
+	/* DisplayPort alternate mode */
+	struct mutex dp_lock;
+	struct tps6598x_dp_sid_status dp_status;
+	bool dp_configured;
+	struct typec_altmode *dp_port;
+	struct typec_altmode *dp_partner;
+	struct work_struct dp_work;
+	u32 dp_vdo_header;
+	bool dp_vdo_send_status;
+};
+
+int tps6598x_block_read(struct tps6598x *tps, u8 reg, void *val, size_t len);
+
+void tps6598x_displayport_update_dp_sid(struct tps6598x *tps);
+int tps6598x_displayport_register_port(struct tps6598x *tps);
+void tps6598x_displayport_unregister_port(struct tps6598x *tps);
+void tps6598x_displayport_unregister_partner(struct tps6598x *tps);
 
 #endif /* __TPS6598X_H__ */
